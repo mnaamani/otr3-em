05ed5f216ee7a343e886ab4f6931373df56f96e4
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2012-12-10 16:08:36 +0300

    adding virtual file system API

--- src/otr_pre.js
+++ src/otr_pre.js
@@ -133,7 +133,7 @@ Module['preRun'].push(function(){
 
     Module["malloc"]=_malloc;
     Module["free"]=_free;
-
+    Module["FS"]=FS;
     //select doesn't really have a place in a JS environment.. since i/o is non-blocking
     _select = (function() {
       return 3;//this means all the three socket sets passed to the function are have sockets ready for reading.
